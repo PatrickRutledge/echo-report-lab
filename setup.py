ba1524f935870d63bce0332175146a8d408148fd
Pat Rutledge <pat@burnfiddlestick.onmicrosoft.com>
2025-07-24 14:29:57 -0500

    Bump version to 0.1.1 for PyPI reupload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="echo-report-lab",
-    version="0.1.0",
+    version="0.1.1",
     author="Patrick Rutledge",
     description="Civic-grade CNN training lab with HTML export and symbolic reporting",
     packages=find_packages(exclude=["tests", "*.ipynb"]),
